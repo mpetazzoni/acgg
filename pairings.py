606fed0e6e61cfd9108fd032e7fac5a646077c2e
Maxime Petazzoni <maxime.petazzoni@bulix.org>
2019-04-06 09:46:07 -0700

    Gather both attendees and assignments and index by member ID

--- pairings.py
+++ pairings.py
@@ -13,35 +13,79 @@ import requests
 import sys
 
 
-BASE_URL = 'https://api.motorsportreg.com/rest/events/{}/assignments'
+ATTENDEES_URL = 'https://api.motorsportreg.com/rest/events/{}/attendees'
+ASSIGNMENTS_URL = 'https://api.motorsportreg.com/rest/events/{}/assignments'
 
 
-def get_assignments(org_id, event_id, auth):
-    logging.debug('Retrieving assignments for %s event %s', org_id, event_id)
+class Event(object):
 
-    event_url = BASE_URL.format(event_id)
-    headers = {'X-Organization-Id': org_id}
-    with requests.get(event_url, auth=auth, headers=headers) as r:
-        r.raise_for_status()
-        soup = BeautifulSoup(r.text, 'html.parser')
-        items = soup.find_all('assignment')
-        logging.debug('Found %d assignments.', len(items))
+    def __init__(self, session, event_id):
+        self._event_id = event_id
+        self._attendees = {}
+        self._assignments = {}
 
-    def f(*fields):
-        return ' '.join([f.text for f in fields]).strip() or None
+        def f(*fields):
+            return ' '.join([f.text for f in fields]).strip() or None
 
-    assignments = {}
-    for item in items:
-        assignments[item.id.text] = {
-            'attendee': f(item.attendeeid),
-            'class': f(item.classshort),
-            'group': f(item.group),
-            'name': f(item.firstname, item.lastname),
-            'car': f(item.make, item.model),
-            'instructor': f(item.instructorfirstname,
-                            item.instructorlastname),
-        }
-    return assignments
+        def member_id(field):
+            uri = f(field)
+            return uri.split('/')[2] if uri else None
+
+        logging.debug('Retrieving attendees for event %s', event_id)
+        with session.get(ATTENDEES_URL.format(event_id)) as r:
+            r.raise_for_status()
+            soup = BeautifulSoup(r.text, 'html.parser')
+            items = soup.find_all('attendee')
+            logging.debug('Found %d attendees.', len(items))
+
+        for item in items:
+            self._attendees[member_id(item.memberuri)] = {
+                'name': f(item.firstname, item.lastname),
+                'status': f(item.status),
+                'email': f(item.email),
+                'first_timer': f(item.isfirstevent) == 'true',
+            }
+
+        logging.debug('Retrieving assignments for event %s', event_id)
+        with session.get(ASSIGNMENTS_URL.format(event_id)) as r:
+            r.raise_for_status()
+            soup = BeautifulSoup(r.text, 'html.parser')
+            items = soup.find_all('assignment')
+            logging.debug('Found %d assignments.', len(items))
+
+        for item in items:
+            member = member_id(item.memberuri)
+            instructor = member_id(item.instructoruri)
+            if instructor and instructor not in self._attendees:
+                logging.error(
+                    'Cannot find instructor %s (%s) in attendees!',
+                    instructor,
+                    f(item.instructorfirstname, item.instructorlastname))
+
+            self._assignments[item.id.text] = {
+                'attendee': self._attendees[member],
+                'group': f(item.groupshort),
+                'class': f(item.classshort),
+                'modifier': f(item.classmodifier),
+                'car': f(item.make, item.model),
+                'instructor': self._attendees.get(instructor),
+            }
+
+    @property
+    def event_id(self):
+        return self._event_id
+
+    def _find(self, predicate):
+        return filter(predicate, self._assignments.values())
+
+    def get_group(self, group):
+        return list(self._find(lambda x: x['group'] == group))
+
+    def get_instructor_students(self, instructor):
+        pass
+
+    def render(self):
+        print(self._assignments)
 
 
 if __name__ == '__main__':
@@ -57,6 +101,11 @@ if __name__ == '__main__':
 
     logging.basicConfig(stream=sys.stderr, level=options.loglevel)
 
-    auth = requests.auth.HTTPBasicAuth(options.username, options.password)
-    data = get_assignments(options.org, options.event, auth)
-    pprint.pprint(data)
+    session = requests.Session()
+    session.auth = requests.auth.HTTPBasicAuth(options.username, options.password)
+    session.headers = {'X-Organization-Id': options.org}
+    event = Event(session, options.event)
+    pprint.pprint(event.get_group('A'))
+    pprint.pprint(event.get_group('B'))
+    pprint.pprint(event.get_group('C'))
+    pprint.pprint(event.get_group('D'))
